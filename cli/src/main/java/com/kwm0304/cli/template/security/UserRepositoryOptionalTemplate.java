47001dfe2157b2a1d656445a0cf2a224f037452b
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-02-28 15:45:51 -0500

    added user repository method

---
+++ cli/src/main/java/com/kwm0304/cli/template/security/UserRepositoryOptionalTemplate.java
@@ -0,0 +1,19 @@
+package com.kwm0304.cli.template.security;
+
+import com.kwm0304.cli.StringUtils;
+
+public class UserRepositoryOptionalTemplate {
+    public String genMethod(String userClass, String parentDirString, String modelDirStr) {
+        String convertedParent = StringUtils.convertPath(parentDirString);
+        String convertedModel = StringUtils.convertPath(modelDirStr);
+        StringBuilder builder = new StringBuilder();
+        builder.append("package ").append(convertedParent).append(";\n\n")
+                .append("import ").append(convertedModel).append(".").append(userClass).append(";\n")
+                .append("import org.springframework.data.jpa.repository.JpaRepository;\n")
+                .append("import java.util.Optional;\n\n")
+                .append("public interface ").append(userClass).append("Repository extends JPARepository<").append(userClass).append(", ").append(userIdType).append("> {\n")
+                .append("    Optional<").append(userClass).append("> findByUsername(String username);\n")
+                .append("}");
+        return builder.toString();
+    }
+}
