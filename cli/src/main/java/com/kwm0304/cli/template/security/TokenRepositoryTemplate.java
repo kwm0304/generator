2982457096bd193c966864635ba21ed368a39e19
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-02-28 10:34:25 -0500

    added tokenrepo template

---
+++ cli/src/main/java/com/kwm0304/cli/template/security/TokenRepositoryTemplate.java
@@ -0,0 +1,27 @@
+package com.kwm0304.cli.template.security;
+
+import com.kwm0304.cli.StringUtils;
+
+public class TokenRepositoryTemplate {
+    public String genTokenRepository(String modelDir, String repositoryDir) {
+        String convertedModel = StringUtils.convertPath(modelDir);
+        String convertedRepository = StringUtils.convertPath(repositoryDir);
+        String tab = "    ";
+        StringBuilder builder = new StringBuilder();
+        builder.append("package ").append(convertedRepository).append(";\n\n")
+                .append("import ").append(convertedModel).append(".Token;\n")
+                .append("import org.springframework.data.jpa.repository.JpaRepository;\n")
+                .append("import org.springframework.data.jpa.repository.Query;\n")
+                .append("import java.util.List;\n")
+                .append("import java.util.Optional;\n\n")
+                .append("public interface TokenRepository extends JPARepository<Token, Integer> {\n")
+                .append(tab).append("@Query(\"\"\"\n")
+                .append(tab).append(tab).append("select t from Token t inner join User u on t.user.id = u.id\n")
+                .append(tab).append(tab).append("where t.user.id = :userId and t.loggedOut = false\n")
+                .append(tab).append(tab).append("\"\"\")\n")
+                .append(tab).append("List<Token> findAllTokensByUser(Integer userId);\n\n")
+                .append(tab).append("Optional<Token> findByToken(String token);\n")
+                .append("}");
+        return builder.toString();
+    }
+}
